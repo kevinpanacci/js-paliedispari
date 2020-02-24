e383168e4bead2a9e1d20ea0070657af6747a0a4
Kevin <kevinpanacci@hotmail.it>
2020-02-24 18:19:42 +0100

    ?

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,1 +1,8 @@
 // Verifica se una parola è palindroma attraverso una funzione
+myString = 'yamamay';
+palindromeString = '';
+
+charAt() // riporta il carattere rispettivo all'indice specificato della stringa
+indexOf()
+
+// utilizzare array e portare questo carattere in array con push? funzionerebbe?
