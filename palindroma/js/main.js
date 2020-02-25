860a025d9b821b0890905b41bec3a65ff4d4787c
Kevin <kevinpanacci@hotmail.it>
2020-02-25 10:05:18 +0100

    Aggiunta soluzione Palindroma vista in aula

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,8 +1,26 @@
 // Verifica se una parola è palindroma attraverso una funzione
-myString = 'yamamay';
-palindromeString = '';
-
-charAt() // riporta il carattere rispettivo all'indice specificato della stringa
-indexOf()
-
+// myString = 'yamamay';
+// palindromeString = '';
+// charAt() // riporta il carattere rispettivo all'indice specificato della stringa
+// indexOf()
 // utilizzare array e portare questo carattere in array con push? funzionerebbe?
+
+// SOLUZIONE VISTA IN CLASSE:
+
+var parolaInserita = prompt('Inserisci una parola');
+var parolaPalindroma = palindroma(parolaInserita);
+console.log(parolaPalindroma);
+
+function palindroma(parola) {
+    var parolaReverse = '';
+    for (var i = (parola.length - 1); i >= 0; i--) {
+        parolaReverse += parola[i];
+    }
+    return parolaReverse;
+}
+
+if (parolaPalindroma == parolaInserita) {
+    console.log('La parola inserita è palindroma');
+} else {
+    console.log('La parola inserita non è palindroma');
+}
